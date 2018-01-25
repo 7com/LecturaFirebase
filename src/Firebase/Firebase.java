11ec1ec7a3207dfae0b2bfbc6865600aa58b7aef
Felipe Valenzuela Cornejo <bl4ckt3ch@gmail.com>
2018-01-25 15:42:19 -0300

    Optimización XLS 25-01-2018
    Cambio descarga de JSON por Libreria Firebase SDK

--- src/Firebase/Firebase.java
+++ src/Firebase/Firebase.java
@@ -5,9 +5,15 @@
  */
 package Firebase;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import static java.lang.Float.parseFloat;
@@ -15,9 +21,6 @@ import static java.lang.Integer.parseInt;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -40,7 +43,8 @@ public class Firebase implements Runnable{
 
     private JTree jTree1;
     private int op;
-    TreePath selPath;
+    private TreePath selPath;
+    private String ruta;
     
     @Override
     public void run() {
@@ -50,13 +54,10 @@ public class Firebase implements Runnable{
             s=(String)s.subSequence(0, s.length()-1);
             String[] lista = s.split(",");
             s=lista[1].substring(1)+"/"+lista[2].substring(1);
-            String temp=s;
+            ruta=s;
             s="https://sapbot-001.firebaseio.com/"+s+"/";
-            try {
-                crearXLS(s,temp);
-            } catch (IOException ex) {
-                Logger.getLogger(Firebase.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            crearXLS();
+
         }
         else
         {
@@ -71,151 +72,130 @@ public class Firebase implements Runnable{
         selPath=t;
     }
     
-    private void crearXLS(String url, String n) throws MalformedURLException, IOException{
-        JProgressBar pb = new JProgressBar(0,100);
-        pb.setPreferredSize(new Dimension(250,40));
-        pb.setString("Preparando...");
-        pb.setStringPainted(true);
-        pb.setValue(0); 
+    private void crearXLS(){
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference(ruta);
+        ArrayList<Motor> motores = new ArrayList<Motor>();
+        ValueEventListener cargarMotores = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot ds) {
+                JProgressBar pb = new JProgressBar(0,100);
+                pb.setPreferredSize(new Dimension(250,40));
+                pb.setString("Preparando...");
+                pb.setStringPainted(true);
+                pb.setValue(0); 
 
-        JPanel center_panel = new JPanel();
-        center_panel.add(pb);
-        
-        JDialog dialog = new JDialog((JFrame)null, "Trabajando ...");
-        dialog.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        dialog.getContentPane().add(center_panel, BorderLayout.CENTER);
-        dialog.pack();
-        dialog.setVisible(true);
-        
-        dialog.setLocationRelativeTo(null); // center on screen
-        dialog.toFront(); // raise above other java windows
-        
-        
-        
-        String temp="",volt="",frec="",enco="";
-        ArrayList<String> motor = new ArrayList<String>();
-        motor.add("Motor 1");
-        motor.add("Motor 2");
-        motor.add("Motor 3");
-        motor.add("Motor 4");
-        motor.add("Motor 5");
-        motor.add("Motor 6");
-        motor.add("Motor 7");
-        HSSFWorkbook excel = new HSSFWorkbook();
-        File f = File.createTempFile("SAPBot",".tmp");
-        for(int i=0;i<7;i++)
-        {   
-            pb.setString("Procesando Motor "+(i+1));
-            String s = url+motor.get(i)+"/Frecuencia/.json";
-            FileUtils.copyURLToFile(new URL(s), f);
-            Scanner scan = new Scanner(f);
-            frec=scan.nextLine();
-            scan.close();
-            
-            s = url+motor.get(i)+"/Posicion/.json";
-            FileUtils.copyURLToFile(new URL(s), f);
-            scan = new Scanner(f);
-            enco=scan.nextLine();
-            scan.close();
-            
-            s = url+motor.get(i)+"/Temperatura/.json";
-            FileUtils.copyURLToFile(new URL(s), f);
-            scan = new Scanner(f);
-            temp=scan.nextLine();
-            scan.close();
-            
-            s = url+motor.get(i)+"/Voltaje/.json";
-            FileUtils.copyURLToFile(new URL(s), f);
-            scan = new Scanner(f);
-            volt=scan.nextLine();
-            scan.close();
-            
-            if (!volt.equalsIgnoreCase("null"))
-            {
-                temp=temp.substring(1, temp.length()-1);
-                volt=volt.substring(1, volt.length()-1);
-                frec=frec.substring(1, frec.length()-1);
-                enco=enco.substring(1, enco.length()-1);
-                temp=temp.replaceAll("\\s","");
-                volt=volt.replaceAll("\\s","");
-                frec=frec.replaceAll("\\s","");
-                enco=enco.replaceAll("\\s","");
-                String[] te,vo,fr,en;
-                te=temp.split(",");
-                vo=volt.split(",");
-                fr=frec.split(",");
-                en=enco.split(",");
-                Sheet hoja = excel.createSheet(motor.get(i));
-                Row fila = hoja.createRow((short)0);
-                fila.createCell(0).setCellValue("Frecuencia");
-                fila.createCell(1).setCellValue("Posicion");
-                fila.createCell(2).setCellValue("Temperatura");
-                fila.createCell(3).setCellValue("Voltaje");
+                JPanel center_panel = new JPanel();
+                center_panel.add(pb);
+
+                JDialog dialog = new JDialog((JFrame)null, "Trabajando ...");
+                dialog.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+                dialog.getContentPane().add(center_panel, BorderLayout.CENTER);
+                dialog.pack();
+                dialog.setVisible(true);
+
+                dialog.setLocationRelativeTo(null); // center on screen
+                dialog.toFront(); // raise above other java windows
                 
-                for (int j=1;j<=en.length;j++)
+                for (int i=1;i<=7;i++){   
+                    DataSnapshot d = ds.child("Motor "+i);
+                    if (d.getValue() != null)
+                        motores.add(d.getValue(Motor.class));
+                }
+
+                HSSFWorkbook excel = new HSSFWorkbook();
+                for (int i=0; i<motores.size();i++)
                 {
-                    fila = hoja.createRow((short)j);
-                    fila.createCell(0).setCellValue(parseFloat(fr[j-1]));
-                    fila.createCell(1).setCellValue(parseInt(en[j-1]));
-                    fila.createCell(2).setCellValue(parseFloat(te[j-1]));
-                    fila.createCell(3).setCellValue(parseFloat(vo[j-1]));
-                }
-            }
-            pb.setValue(pb.getValue()+14);
-        }
-        JFileChooser chooser = new JFileChooser();
-        chooser.addChoosableFileFilter(new FileFilter() {
-
-            public String getDescription() {
-                return "Archivo Formato XLS (*.xls)";
-            }
-
-            public boolean accept(File f) {
-                if (f.isDirectory()) {
-                    return true;
-                } else {
-                    return f.getName().toLowerCase().endsWith(".xls");
-                }
-            }
-        });
-        chooser.setAcceptAllFileFilterUsed(false);
-        n=n.replaceAll("/", " - ");
-        n=n.replaceAll(":", "_");
-        chooser.setSelectedFile(new File(n));
-        if (chooser.showSaveDialog(jTree1) == JFileChooser.APPROVE_OPTION)
-        {
-            pb.setString("Creando XLS");
-            pb.setValue(99);
-            File archivo = chooser.getSelectedFile();
-            if (!archivo.getAbsolutePath().endsWith(".xls")){
-                archivo = new File(chooser.getSelectedFile() + ".xls");
-            }
-            if (archivo.exists()) {
-                int response = JOptionPane.showConfirmDialog(null, //
-                        "¿Desea sobreescribir el archivo?", //
-                        "El archivo ya existe", JOptionPane.YES_NO_OPTION, //
-                        JOptionPane.QUESTION_MESSAGE);
-                if (response == JOptionPane.YES_OPTION) {
-                    try (FileOutputStream fileOut = new FileOutputStream(archivo)) {
-                    excel.write(fileOut);
+                    pb.setString("Procesando Motor "+(i+1));
+                    String[] temp,volt,enco;
+                    temp=motores.get(i).getTemperatura().split(",");
+                    volt=motores.get(i).getVoltaje().split(",");
+                    enco=motores.get(i).getPosicion().split(",");
+                    Sheet hoja = excel.createSheet("Motor "+(i+1));
+                    Row fila = hoja.createRow((short)0);
+                    fila.createCell(0).setCellValue("Posicion");
+                    fila.createCell(1).setCellValue("Temperatura");
+                    fila.createCell(2).setCellValue("Voltaje");
+                    for (int j=1;j<=enco.length;j++)
+                    {
+                        fila = hoja.createRow((short)j);
+                        fila.createCell(0).setCellValue(parseInt(enco[j-1]));
+                        fila.createCell(1).setCellValue(parseFloat(temp[j-1]));
+                        fila.createCell(2).setCellValue(parseFloat(volt[j-1]));
                     }
-                    if (archivo.exists()){
-                        JOptionPane.showMessageDialog(jTree1,"El archivo "+archivo.getName()+" ha sido creado.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                    pb.setValue(pb.getValue()+14);
+                }
+                JFileChooser chooser = new JFileChooser();
+                chooser.addChoosableFileFilter(new FileFilter() {
+
+                    public String getDescription() {
+                        return "Archivo Formato XLS (*.xls)";
                     }
-                } 
-            }
-            else
-            {
-                try (FileOutputStream fileOut = new FileOutputStream(archivo)) {
-                        excel.write(fileOut);
+
+                    public boolean accept(File f) {
+                        if (f.isDirectory()) {
+                            return true;
+                        } else {
+                            return f.getName().toLowerCase().endsWith(".xls");
+                        }
+                    }
+                });
+                chooser.setAcceptAllFileFilterUsed(false);
+                ruta=ruta.replaceAll("/", " - ");
+                ruta=ruta.replaceAll(":", "_");
+                chooser.setSelectedFile(new File(ruta));
+                if (chooser.showSaveDialog(jTree1) == JFileChooser.APPROVE_OPTION)
+                {
+                    pb.setString("Creando XLS");
+                    pb.setValue(99);
+                    File archivo = chooser.getSelectedFile();
+                    if (!archivo.getAbsolutePath().endsWith(".xls")){
+                        archivo = new File(chooser.getSelectedFile() + ".xls");
+                    }
+                    if (archivo.exists()) {
+                        int response = JOptionPane.showConfirmDialog(null, //
+                                "¿Desea sobreescribir el archivo?", //
+                                "El archivo ya existe", JOptionPane.YES_NO_OPTION, //
+                                JOptionPane.QUESTION_MESSAGE);
+                        if (response == JOptionPane.YES_OPTION) {
+                            archivo.delete();
+                            try (FileOutputStream fileOut = new FileOutputStream(archivo)) {
+                                excel.write(fileOut);
+                            } catch (FileNotFoundException ex) {
+                                JOptionPane.showMessageDialog(null,ex.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+                            } catch (IOException ex) {
+                                JOptionPane.showMessageDialog(null,ex.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+                            }
+                            if (archivo.exists()){
+                                JOptionPane.showMessageDialog(jTree1,"El archivo "+archivo.getName()+" ha sido creado.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                            }
+                        } 
+                    }
+                    else
+                    {
+                        try (FileOutputStream fileOut = new FileOutputStream(archivo)) {
+                            excel.write(fileOut);
+                        } catch (FileNotFoundException ex) {
+                            JOptionPane.showMessageDialog(null,ex.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+                        } catch (IOException ex) {
+                            JOptionPane.showMessageDialog(null,ex.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+                        }
+                        if (archivo.exists()){
+                            JOptionPane.showMessageDialog(jTree1,"El archivo "+archivo.getName()+" ha sido creado.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                        }
+                    }
                 }
-                if (archivo.exists()){
-                    JOptionPane.showMessageDialog(jTree1,"El archivo "+archivo.getName()+" ha sido creado.","Aviso",JOptionPane.INFORMATION_MESSAGE);
-                }
+                dialog.dispose();
+                
             }
-        }
-        dialog.dispose();
-        f.delete();
+
+            @Override
+            public void onCancelled(DatabaseError de) {
+                JOptionPane.showMessageDialog(null,de.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+            }
+        };
+        ref.addListenerForSingleValueEvent(cargarMotores);
+        
     }
     
     private void descargarJSON(){
@@ -257,6 +237,7 @@ public class Firebase implements Runnable{
                     JOptionPane.QUESTION_MESSAGE);
                 if (response == JOptionPane.YES_OPTION) {
                     try {
+                        archivo.delete();
                         FileUtils.copyURLToFile(new URL(s), archivo);
                     } catch (MalformedURLException ex) {
                         JOptionPane.showMessageDialog(null, ex.getMessage());
--- src/main.java
+++ src/main.java
@@ -1,10 +1,47 @@
 
 import GUI.Pantalla;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class main {
+    private static String BD_URL, CREDENCIALES;
     public static void main(String args[]) {
+        try {
+            carcarCFG();
+            iniciarFirebase();
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(null,ex.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
         Pantalla p = new Pantalla();
         p.setVisible(true);
     }
     
+    public static void carcarCFG() throws FileNotFoundException{
+        File archivo = new File("config.cfg");
+        Scanner scan = new Scanner(archivo);
+        BD_URL=scan.nextLine();
+        CREDENCIALES=scan.nextLine();
+        if (BD_URL == null || CREDENCIALES == null)
+            throw new FileNotFoundException("Archivo config.cfg se encuentra incompleto./n"
+                    + "Revisar archivo config.cfg");
+    }
+    
+    public static void iniciarFirebase() throws FileNotFoundException{
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setServiceAccount(new FileInputStream(CREDENCIALES))
+                    .setDatabaseUrl(BD_URL)
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (FileNotFoundException ex) {
+            throw new FileNotFoundException("El archivo "+CREDENCIALES+" no existe.\n"
+                        + "Revisar archivo config.cfg");
+        } 
+    }
 }
